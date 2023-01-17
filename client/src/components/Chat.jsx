8fecadfbd1c06be5f7c6e038b97864336415e858
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2023-01-17 13:47:12 +0300

    настрола роуты

---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+const Chat = () => {
+    return (
+        <div>
+            Chat
+        </div>
+    )
+
+};
+
+export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const http = require('http');
-const {Server} = require ('socket.io');
+const { Server } = require('socket.io');
 const cors = require('cors');
 const app = express();
 
 const route = require('./route');
 
-app.use(cors({origin:"*"}));
+app.use(cors({ origin: "*" }));
 app.use(route)
 const server = http.createServer(app);
 
-server.listen(5000, ()=>{
+const io = new Server(server,{
+    cors: {
+        origin: "*",
+        metods: ["GET", "POST"],
+    },
+});
+
+server.listen(5000, () => {
     console.log('server is running')
 });
