97a31cdf911f0fa99dcbc0ba656eb07ced2a3949
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2023-01-18 13:36:11 +0300

    join, приветствие юзера

--- server/index.js
+++ server/index.js
@@ -10,11 +10,25 @@ app.use(cors({ origin: "*" }));
 app.use(route)
 const server = http.createServer(app);
 
-const io = new Server(server,{
+const io = new Server(server, {
     cors: {
         origin: "*",
         metods: ["GET", "POST"],
     },
+});
+
+io.on('connection', (socket) => {
+    socket.on('join', ({ name, room }) => {
+        socket.join(room);
+
+        socket.emit('message', {
+            data: {user: {name: 'Admin'}, message: `Hey, ${name}`}
+        })
+    })
+
+    io.on('disconnect', () => {
+        console.log('Disconnect')
+    });
 });
 
 server.listen(5000, () => {
